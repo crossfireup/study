5be12111f6c51d43228f524cf2051f0f07eae780
root <root@Dobly-PC.localdomain>
2017-07-13 23:37:29 +0800

    c tips

---
+++ system/c/src/typeof.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    size_t n = strtoull(argv[1], 0, 0);
+    double A[n][n];
+
+    return 0;
+}
